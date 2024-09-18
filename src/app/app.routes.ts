33ea34ce29a1b64b297bcc3df6543c0be0147b72
fatoumata dansoko <fatoumatadansoko61@gmail.com>
2024-09-18 17:27:28 +0000

    implémentation du header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './Composants/Commun/login/login.component';
+import { RegisterComponent } from './Composants/Commun/register/register.component';
+import { HeaderComponent } from './Composants/Commun/header/header.component';
+import { FooterComponent } from './Composants/Commun/footer/footer.component';
+import { AcceuilComponent } from './Composants/Visiteur/acceuil/acceuil.component';
+import { SidebarComponent } from './Composants/Commun/sidebar/sidebar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'Acceuil', pathMatch: 'full' },
+    
+
+    // Routes pour l'authentification
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+
+
+   // Routes pour les visiteurs
+   { path: 'header', component: HeaderComponent },
+   { path: 'footer', component: FooterComponent},
+   { path: 'acceuil', component: AcceuilComponent},
+   { path: 'sidebar', component: SidebarComponent}
+]
